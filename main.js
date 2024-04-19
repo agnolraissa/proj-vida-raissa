8a7b22b1a69feb3f91d5802868e4ddaa06c873c8
raissa <108005454+agnolraissa@users.noreply.github.com>
2024-04-19 10:45:36 -0300

    19/04

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
     const botoes = document.querySelectorAll(".ativo");
     const textos = document.querySelectorAll(".aba-conteudo");
-
     for (let i=0;i<botoes.length; i++) {
         botoes[i].onclick = function(){
 
@@ -14,4 +13,26 @@
         }
     }
         
-    
+ const contadores = document.querySelector(" .contador");
+ 
+const tempoObjetivo1 = new Date("2024-07-05T00:00:00");
+
+const tempoAtual = Date();
+
+ contadores[0].texContent = calculaTempo (tempoObjetivo1);
+ 
+
+ function calculaTempo (tempoObjetivo){
+    let tempoAtual = new Date ()
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ 
+}
